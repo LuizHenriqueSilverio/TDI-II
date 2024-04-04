e63f9427e0b62f373aa06e72a4748148c6d6e38d
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2024-04-04 10:48:37 -0300

    Atualizado - 04/04/2024

---
+++ Aulas/typeof e instanceof/typeof.js
@@ -0,0 +1,14 @@
+let type = typeof 10;
+console.log(type);
+console.log(typeof type);
+
+let x = new String("A");
+console.log(typeof x);
+
+let und1;
+let und2 = undefined;
+let null1 = null;
+
+console.log(typeof und1);
+console.log(typeof und2);
+console.log(typeof null1); // irá retornar object, um bug da linguagem
