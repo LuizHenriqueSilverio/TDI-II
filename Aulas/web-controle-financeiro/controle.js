6fc8e31189a37064bbe41dd5a9ba08d26773ae03
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2024-04-12 07:43:49 -0300

    Atualizado - aula 11/04/2024

--- Aulas/web-controle-financeiro/controle.js
+++ Aulas/web-controle-financeiro/controle.js
@@ -2,6 +2,8 @@ const form = document.getElementById('form');
 const descInput = document.getElementById('descricao');
 const valueInput = document.querySelector('#montante');
 const balance = document.getElementById('balanco');
+const revenue = document.querySelector('#din-positivo');
+const expenses = document.querySelector('#din-negativo');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -24,8 +26,12 @@ form.addEventListener('submit', (event) => {
     const transaction = {
         id: parseInt(Math.random() * 10000),
         desc: descTransaction,
-        value: valueTransaction
+        value: parseFloat(valueTransaction),
     }
+
+    sumToBalance(transaction);
+
+    sumRevenueExpenses(transaction);
 
     descInput.value = '';
     valueInput.value = '';
@@ -35,4 +41,18 @@ function sumToBalance(transaction) {
     let balanceValue = balance.innerHTML.trim();
     balanceValue = balanceValue.replace('R$', '');
     balanceValue = parseFloat(balanceValue); 
+    balanceValue += transaction.value;
+
+    balance.innerHTML = `R$${balanceValue.toFixed(2)}`;
+}
+
+function sumRevenueExpenses(transaction) {
+    const element = transaction.value > 0 ? revenue : expenses;
+    const sub = transaction.value > 0 ? '+ R$' : '- R$';
+    
+    let value = element.innerHTML.replace(sub, '');
+    value = parseFloat(value);
+    value += Math.abs(transaction.value);
+
+    element.innerHTML = `${sub}${value.toFixed(2)}`;
 }
