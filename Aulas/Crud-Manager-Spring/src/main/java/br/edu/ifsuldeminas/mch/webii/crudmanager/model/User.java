616e75a3fcc61bc5ab529c4abc009add3ddfd293
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2024-08-08 10:51:38 -0300

    Atualização - aula 08/08/2024

---
+++ Aulas/Crud-Manager-Spring/src/main/java/br/edu/ifsuldeminas/mch/webii/crudmanager/model/User.java
@@ -0,0 +1,31 @@
+package br.edu.ifsuldeminas.mch.webii.crudmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User() {}
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String gender;
+
+    private String email;
+
+}
